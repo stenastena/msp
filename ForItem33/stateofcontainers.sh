b59257e2fd0cb3407aeb39d0f4c6c9abb844808a
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-24 15:49:50 +0300

    We are getting the dockerized containers !@!@!@

--- ForItem33/stateofcontainers.sh
+++ ForItem33/stateofcontainers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#docker ps -a
 echo "We are getting the Network properties !#!#!#!"
 sudo ifconfig
 echo "We are getting the Hostname!!!"
 sudo hostname
+echo "We are getting the dockerized containers !@!@!@"
+sudo docker ps -a
