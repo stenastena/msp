c28dd27da0e835254f1464c40680c3585c67a11d
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 00:04:47 +0300

    .......

--- ForItem33/wp.sh
+++ ForItem33/wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 sudo snap install docker
 sleep 5s
-sudo docker run --name wp007 -p 8080:80 -e WORDPRESS_DB_HOST=mysqlmsp.mysql.database.azure.com:3306 \
--e WORDPRESS_DB_USER=superadmin@mysqlmsp -e WORDPRESS_DB_PASSWORD=1q21!Q@!1q21 \
--e WORDPRESS_DB_NAME=wp -d wordpress
+sudo docker run --name wp007 -p 8080:80 -e WORDPRESS_DB_HOST=wordpressmsp-mysqlserver.mysql.database.azure.com:3306 \
+-e WORDPRESS_DB_USER=superadmin@wordpressmsp-mysqlserver -e WORDPRESS_DB_PASSWORD=1q21!Q@!1q21 \
+-e WORDPRESS_DB_NAME=wordpressmsp-database -d wordpress
 
