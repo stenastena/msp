026f951c3132a341a05ea9cce9f5a88e1e1ccb3f
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-24 10:09:14 +0300

    new scripts for run container inside Linux VM

---
+++ ForItem33/wp.sh
@@ -0,0 +1,1 @@
+sudo docker run --name wp02 -p 8080:80 -e WORDPRESS_DB_HOST=mysqlmsp.mysql.database.azure.com:3306 -e WORDPRESS_DB_USER=superadmin@mysqlmsp -e WORDPRESS_DB_PASSWORD=1q21!Q@!1q21 -e WORDPRESS_DB_NAME=wp -d wordpress
