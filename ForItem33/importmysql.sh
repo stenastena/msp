befa354f9a686bcccc14f4336f1154210edbef0e
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 07:58:02 +0300

    add installing MySQL

--- ForItem33/importmysql.sh
+++ ForItem33/importmysql.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
+sudo apt update
+sudo apt install mysql-server
+
 mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
 -p1q21!Q@!1q21 -e "CREATE DATABASE wordpressmspdb"
 
