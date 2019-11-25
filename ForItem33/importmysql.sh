310b621512e5732ea6b1599188b0019ec4f455c9
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 10:39:47 +0300

    Comment all commands for importing database

--- ForItem33/importmysql.sh
+++ ForItem33/importmysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
-sudo apt update -y
-sudo apt install mysql-server -y
+#sudo apt update -y
+#sudo apt install mysql-server -y
 
-mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
+#mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
 -p1q21!Q@!1q21 -e "CREATE DATABASE wordpressmspdb"
 
-cd ~
-wget https://raw.githubusercontent.com/stenastena/msp/master/ForItem33/Dump20191124.sql
+#cd ~
+#wget https://raw.githubusercontent.com/stenastena/msp/master/ForItem33/Dump20191124.sql
 
-mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
+#mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
 -p1q21!Q@!1q21 wordpressmspdb < ~/Dump20191124.sql > output.log
