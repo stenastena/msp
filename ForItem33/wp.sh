37396eef3290a45113684185065b0889e7de7d5d
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 10:33:37 +0300

    add autolstart Wordpress container after Web Server reboot

--- ForItem33/wp.sh
+++ ForItem33/wp.sh
@@ -5,3 +5,8 @@ sudo docker run --name wp007 -p 8080:80 -e WORDPRESS_DB_HOST=wordpressmsp-mysqls
 -e WORDPRESS_DB_USER=superadmin@wordpressmsp-mysqlserver -e WORDPRESS_DB_PASSWORD=1q21!Q@!1q21 \
 -e WORDPRESS_DB_NAME=wordpressmspdb -d wordpress
 
+#making autostartung container with Wordpress
+cd /etc/init.d
+sudo curl -O https://raw.githubusercontent.com/stenastena/msp/master/ForItem33/wps.sh
+sudo chmod +x /etc/init.d/wps.sh
+sudo update-rc.d wps.sh defaults 80
