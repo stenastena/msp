bf7cb86680668b880b11560b48ea1872df73adde
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 10:50:53 +0300

    ........

--- ForItem33/importmysql.sh
+++ ForItem33/importmysql.sh
@@ -3,10 +3,10 @@
 #sudo apt install mysql-server -y
 
 #mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
--p1q21!Q@!1q21 -e "CREATE DATABASE wordpressmspdb"
+#-p1q21!Q@!1q21 -e "CREATE DATABASE wordpressmspdb"
 
 #cd ~
 #wget https://raw.githubusercontent.com/stenastena/msp/master/ForItem33/Dump20191124.sql
 
 #mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
--p1q21!Q@!1q21 wordpressmspdb < ~/Dump20191124.sql > output.log
+#-p1q21!Q@!1q21 wordpressmspdb < ~/Dump20191124.sql > output.log
