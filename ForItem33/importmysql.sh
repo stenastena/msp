e0e969635cb4558a0df3638d4c21380f02198d86
Sergey.Osherov <sergey_osherov@mail.ru>
2019-11-25 08:47:57 +0300

    dowloading dump MySQL

--- ForItem33/importmysql.sh
+++ ForItem33/importmysql.sh
@@ -5,7 +5,8 @@ sudo apt install mysql-server
 mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
 -p1q21!Q@!1q21 -e "CREATE DATABASE wordpressmspdb"
 
-!надо загрузить дамп локально! 
+cd ~
+wget https://raw.githubusercontent.com/stenastena/msp/master/ForItem33/Dump20191124.sql
 
 mysql -h wordpressmsp-mysqlserver.mysql.database.azure.com -u superadmin@wordpressmsp-mysqlserver \
 -p1q21!Q@!1q21 wordpressmspdb < ~/Dump20191124.sql > output.log
